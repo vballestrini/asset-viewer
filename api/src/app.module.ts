fc64c511d1c6a42cf0a561ce6ae9b6c4798be6d2
Víctor Ballestrini <vsballestrini@gmail.com>
2025-03-03 11:24:57 -0300

    feat: update app module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AssetsModule } from './assets/assets.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { WalletsModule } from './wallets/wallets.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://root:root@localhost:27017/nest?authSource=admin&directConnection=true',
+    ),
+    AssetsModule,
+    WalletsModule,
+    OrdersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
